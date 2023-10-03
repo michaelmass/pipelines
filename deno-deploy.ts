56814183b303bb0e8c3b8299b461db55a7c5ab59
michaelmass <10071388+michaelmass@users.noreply.github.com>
2023-10-03 13:43:17 -0400

    update

---
+++ deno-deploy.ts
@@ -0,0 +1,51 @@
+import { Client } from "https://esm.sh/@dagger.io/dagger@0.8.7";
+
+type DeployOptions = {
+  /**
+   * The dagger client to use
+   */
+  client: Client;
+  /**
+   * The directory to use as the source for the deploy
+   * @default .
+   */
+  dir?: string;
+  /**
+   * Whether or not to deploy to production
+   * @default true
+   */
+  prod?: boolean;
+  /**
+   * The deno deploy project name
+   */
+  project: string
+  /**
+   * The deno deploy token
+   */
+  deployToken: string;
+  /**
+   * The entrypoint to use for the deploy
+   * @default main.ts
+   */
+  entrypoint?: string;
+}
+
+export async function denoDeploy({ client, dir = ".", prod = true, project, entrypoint = "main.ts", deployToken }: DeployOptions) {
+  const directory = client.host().directory(dir)
+
+  const denoDeployToken = client.setSecret(
+    "denoDeployToken",
+    deployToken,
+  )
+
+  await client
+    .pipeline("deploy")
+    .container()
+    .from("denoland/deno")
+    .withSecretVariable("DENO_DEPLOY_TOKEN", denoDeployToken)
+    .withDirectory("/src", directory)
+    .withWorkdir("/src")
+    .withExec(["deno", "install", "-Arf", "https://deno.land/x/deploy/deployctl.ts"], { skipEntrypoint: true })
+    .withExec(["deployctl", "deploy", `--project=${project}`, prod ? "--prod" : "", entrypoint], { skipEntrypoint: true })
+    .sync();
+}
