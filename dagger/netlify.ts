d2ec9cc70558ccfdb26a1e0d151df0c94d423ed9
Michael Masson <10071388+michaelmass@users.noreply.github.com>
2023-11-05 14:44:27 -0500

    feat(netlify): deploy netlify (#9)

---
+++ dagger/netlify.ts
@@ -0,0 +1,50 @@
+import {
+  Client,
+  Directory,
+  Secret,
+} from "https://esm.sh/@dagger.io/dagger@0.8.7";
+
+type DeployNetlifyOptions = {
+  /**
+   * The dagger client to use
+   */
+  client: Client;
+  /**
+   * The directory to use as the source for the deploy
+   */
+  dir?: Directory | string;
+  /**
+   * Netlify token to use for authentication
+   */
+  token: Secret;
+  /**
+   * The netlify site to deploy to
+   */
+  site: string;
+};
+
+export async function deploy(
+  {
+    client,
+    dir = ".",
+    token,
+    site,
+  }: DeployNetlifyOptions,
+) {
+  const directory = typeof dir === "string"
+    ? client.host().directory(dir)
+    : dir;
+
+  await client
+    .pipeline("deploy")
+    .container()
+    .from("node:20-slim")
+    .withExec(["npm", "install", "-g", "netlify-cli"], { skipEntrypoint: true })
+    .withDirectory("/app", directory)
+    .withEnvVariable("NETLIFY_SITE_ID", site)
+    .withSecretVariable("NETLIFY_AUTH_TOKEN", token)
+    .withExec(["netlify", "deploy", "--prod", "--dir", "/app"], {
+      skipEntrypoint: true,
+    })
+    .sync();
+}
