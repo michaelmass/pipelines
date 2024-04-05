ba59a1e8cd7bc4810c784f8b333c1fa1d064db39
Michael Masson <10071388+michaelmass@users.noreply.github.com>
2024-04-05 18:08:57 -0400

    feat(helm&cloudflare): added helm & cloudflare pipelines (#19)

---
+++ dagger/cloudflare.ts
@@ -0,0 +1,88 @@
+import {
+  Client,
+  Directory,
+  Secret,
+} from "./dagger.ts";
+
+type UploadOptions = {
+  /**
+   * The dagger client to use
+   */
+  client: Client;
+  /**
+   * The directory to use as the source for the deploy
+   * @default .
+   */
+  dir?: string | Directory;
+  /**
+   * The name of the project you want to deploy to
+   */
+  project: string;
+  /**
+   * The name of the branch you want to deploy to
+   */
+  branch?: string;
+  /**
+   * The SHA to attach to this deployment
+   */
+  commitHash?: string;
+  /**
+   * The commit message to attach to this deployment
+   */
+  commitMessage?: string;
+  /**
+   * Whether or not the workspace should be considered dirty for this deployment
+   */
+  commitDirty?: boolean;
+  /**
+   * Cloudflare API token
+   */
+  cloudflareToken: Secret;
+  /**
+   * Cloudflare account ID
+   */
+  accountId: Secret;
+};
+
+export async function upload({
+  client,
+  dir = ".",
+  project,
+  cloudflareToken,
+  accountId,
+  branch,
+  commitHash,
+  commitMessage,
+  commitDirty,
+}: UploadOptions) {
+  const directory = typeof dir === "string"
+    ? client.host().directory(dir)
+    : dir;
+
+  const exec = [
+    "wrangler",
+    "pages",
+    "publish",
+    ".",
+    "--project-name",
+    project,
+    branch ? "--branch" : undefined,
+    branch || undefined,
+    commitHash ? "--commit-hash" : undefined,
+    commitHash || undefined,
+    commitMessage ? "--commit-message" : undefined,
+    commitMessage || undefined,
+    commitDirty ? "--commit-dirty" : undefined,
+  ].filter((v) => v !== undefined) as string[]
+
+  await client
+    .pipeline("upload")
+    .container()
+    .from("michaelmass/wrangler:0.0.1")
+    .withSecretVariable("CLOUDFLARE_ACCOUNT_ID", cloudflareToken)
+    .withSecretVariable("CLOUDFLARE_API_TOKEN", accountId)
+    .withDirectory("/src", directory)
+    .withWorkdir("/src")
+    .withExec(exec, { skipEntrypoint: true })
+    .sync();
+}
---
+++ dagger/helm.ts
@@ -0,0 +1,123 @@
+import {
+  Client,
+  File,
+} from "./dagger.ts";
+import { stringify } from "https://deno.land/std@0.221.0/yaml/mod.ts";
+
+type DeployHelmOptions = {
+  /**
+   * The dagger client to use
+   */
+  client: Client;
+  /**
+   * Chart name to use for deployment
+   */
+  chart: string;
+  /**
+   * Release name to use for the deployment
+   */
+  release: string;
+  /**
+   * Namespace to deploy the chart to
+   */
+  namespace: string;
+  /**
+   * Values file to use for the deployment
+   */
+  values: string | Record<string, unknown> | File;
+  /**
+   * Repository to use for the deployment
+   */
+  repository: string;
+  /**
+   * Version of the chart to deploy
+   */
+  version: string;
+  /**
+   * Atomic flag for the deployment
+   */
+  atomic?: boolean;
+  /**
+   * Create namespace flag for the deployment
+   */
+  createNamespace?: boolean;
+  /**
+   * Cleanup on fail flag for the deployment
+   */
+  cleanupOnFail?: boolean;
+  /**
+   * Max history for the deployment
+   */
+  maxHistory?: number;
+  /**
+   * Timeout for the deployment in seconds
+   */
+  timeout?: number;
+  /**
+   * Kubeconfig file to use for the deployment or the content of the kubeconfig file
+   */
+  kubeconfig: string | File;
+};
+
+export async function deploy(
+  {
+    client,
+    maxHistory = 10,
+    kubeconfig,
+    namespace,
+    chart,
+    release,
+    values,
+    repository,
+    version,
+    atomic = true,
+    cleanupOnFail = true,
+    createNamespace = false,
+    timeout = 300,
+  }: DeployHelmOptions,
+) {
+  const kubeconfigPath = "/deploy/kubeconfig";
+  const valuesPath = "/deploy/values.yaml";
+
+  const kubeconfigFile = typeof kubeconfig === "string" ?
+    client.directory().withNewFile(kubeconfigPath, kubeconfig).file(kubeconfigPath) :
+    kubeconfig
+
+  const valuesFile = values instanceof File ?
+    values :
+    client.directory().withNewFile(valuesPath, values === "string" ? values : stringify(values)).file(valuesPath)
+
+  const exec = [
+    "helm",
+    "upgrade",
+    release,
+    chart,
+    "--install",
+    "--timeout",
+    timeout.toString(),
+    "--version",
+    version,
+    "--repo",
+    repository,
+    "--namespace",
+    namespace,
+    "--kubeconfig",
+    kubeconfigPath,
+    "--history-max",
+    maxHistory.toString(),
+    "--values",
+    valuesPath,
+    atomic ? "--atomic" : undefined,
+    cleanupOnFail ? "--cleanup-on-fail" : undefined,
+    createNamespace ? "--create-namespace" : undefined,
+  ].filter((v) => v !== undefined) as string[]
+
+  await client
+    .pipeline("deploy")
+    .container()
+    .from("helm:3.14.3")
+    .withFile(kubeconfigPath, kubeconfigFile)
+    .withFile(valuesPath, valuesFile)
+    .withExec(exec, { skipEntrypoint: true })
+    .sync();
+}
