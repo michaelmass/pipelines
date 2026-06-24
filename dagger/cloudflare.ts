905314d872a108346a80864d297e29f1736b238e
Michael Masson <10071388+michaelmass@users.noreply.github.com>
2026-06-24 18:28:14 -0400

    refactor(cloudflare): upgrade Wrangler image to 0.0.6 and improve upload logic (#54)

--- dagger/cloudflare.ts
+++ dagger/cloudflare.ts
@@ -1,6 +1,6 @@
 import type { Client, Directory, Secret } from './dagger.ts'
 
-const wranglerImage = 'michaelmass/wrangler:0.0.5'
+const wranglerImage = 'michaelmass/wrangler:0.0.6'
 
 type UploadOptions = {
   /**
@@ -45,7 +45,76 @@ type UploadOptions = {
 export async function upload({ client, dir = '.', project, cloudflareToken, accountId, branch, commitHash, commitMessage, commitDirty }: UploadOptions) {
   const directory = typeof dir === 'string' ? client.host().directory(dir) : dir
 
-  const exec = [
+  const exec = formatUploadCommad({ project, branch, commitHash, commitMessage, commitDirty })
+
+  await client
+    .container()
+    .from(wranglerImage)
+    .withSecretVariable('CLOUDFLARE_ACCOUNT_ID', accountId)
+    .withSecretVariable('CLOUDFLARE_API_TOKEN', cloudflareToken)
+    .withDirectory('/src', directory)
+    .withWorkdir('/src')
+    .withExec(exec)
+    .sync()
+}
+
+type ContainerOptions = {
+  /**
+   * The dagger client to use
+   */
+  client: Client
+  /**
+   * The directory to use as the source for the deploy
+   * @default .
+   */
+  dir?: string | Directory
+  /**
+   * Cloudflare API token
+   */
+  cloudflareToken: Secret
+  /**
+   * Cloudflare account ID
+   */
+  accountId: Secret
+}
+
+export function container({ client, dir = '.', cloudflareToken, accountId }: ContainerOptions) {
+  const directory = typeof dir === 'string' ? client.host().directory(dir) : dir
+
+  return client
+    .container()
+    .from(wranglerImage)
+    .withSecretVariable('CLOUDFLARE_ACCOUNT_ID', accountId)
+    .withSecretVariable('CLOUDFLARE_API_TOKEN', cloudflareToken)
+    .withDirectory('/src', directory)
+    .withWorkdir('/src')
+}
+
+type FormatUploadCommadOptions = {
+  /**
+   * The name of the project you want to deploy to
+   */
+  project: string
+  /**
+   * The name of the branch you want to deploy to
+   */
+  branch?: string
+  /**
+   * The SHA to attach to this deployment
+   */
+  commitHash?: string
+  /**
+   * The commit message to attach to this deployment
+   */
+  commitMessage?: string
+  /**
+   * Whether or not the workspace should be considered dirty for this deployment
+   */
+  commitDirty?: boolean
+}
+
+export function formatUploadCommad({ project, branch, commitHash, commitMessage, commitDirty }: FormatUploadCommadOptions) {
+  return [
     'wrangler',
     'pages',
     'deploy',
@@ -60,16 +129,6 @@ export async function upload({ client, dir = '.', project, cloudflareToken, acco
     commitMessage || undefined,
     commitDirty ? '--commit-dirty' : undefined,
   ].filter(v => v !== undefined) as string[]
-
-  await client
-    .container()
-    .from(wranglerImage)
-    .withSecretVariable('CLOUDFLARE_ACCOUNT_ID', accountId)
-    .withSecretVariable('CLOUDFLARE_API_TOKEN', cloudflareToken)
-    .withDirectory('/src', directory)
-    .withWorkdir('/src')
-    .withExec(exec)
-    .sync()
 }
 
 type WhoAmIOptions = {
